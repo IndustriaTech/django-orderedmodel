69a174d219c889ff9baee2b39e5b729fc9294d72
Venelin Stoykov <venelin@magicbg.com>
2012-11-07 13:49:29 +0200

    Add OrderedModelManager
    swap and max_order methods must be in model manager not in model
    because they operate over objects

--- orderedmodel/models.py
+++ orderedmodel/models.py
@@ -2,8 +2,25 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 
+class OrderedModelManager(models.Manager):
+    def swap(self, obj1, obj2):
+        tmp, obj2.order = obj2.order, 0
+        obj2.save(swapping=True)
+        obj2.order, obj1.order = obj1.order, tmp
+        obj1.save()
+        obj2.save()
+
+    def max_order(self):
+        try:
+            return self.order_by('-order').values_list('order', flat=True)[0]
+        except IndexError:
+            return 0
+
+
 class OrderedModel(models.Model):
     order = models.PositiveIntegerField(blank=True, default=1, db_index=True)
+
+    objects = OrderedModelManager()
 
     class Meta:
         abstract = True
@@ -18,15 +35,8 @@ class OrderedModel(models.Model):
 
     @classmethod
     def swap(cls, obj1, obj2):
-        tmp, obj2.order = obj2.order, 0
-        obj2.save(swapping=True)
-        obj2.order, obj1.order = obj1.order, tmp
-        obj1.save()
-        obj2.save()
+        cls.objects.swap(obj1, obj2)
 
     @classmethod
     def max_order(cls):
-        try:
-            return cls.objects.order_by('-order').values_list('order', flat=True)[0]
-        except IndexError:
-            return 0
+        return cls.objects.max_order()
