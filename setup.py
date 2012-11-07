772c51aa7c74a5e6a88af44f449b055a92c138d7
Venelin Stoykov <venelin@magicbg.com>
2012-11-07 11:57:17 +0200

    Fix typo
    The application is orderedmodel not ordermodel

--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ def read(fname):
 README = read('README.md')
 
 setup(
-    name="django-ordermodel",
+    name="django-orderedmodel",
     version="0.0.1",
-    url='http://github.com/MagicSolutions/django-ordermodel',
+    url='http://github.com/MagicSolutions/django-orderedmodel',
     description="Intends to help you create models which can bemoved up\down (or left\right) with respect to each other.",
     long_description=README,
 
     author='Kirill Elagin',
     author_email='kirelagin@gmail.com',
     packages=[
-        'ordermodel',
+        'orderedmodel',
     ],
     package_data={
-        'ordermodel': [
-            'static/ordermodel/*.gif',
+        'orderedmodel': [
+            'static/orderedmodel/*.gif',
         ],
     },
     classifiers=[
