3a8139ddc17125f514c959cd4e0f9953756fdc28
Venelin Stoykov <venelin@magicbg.com>
2012-11-19 13:26:23 +0200

    Add __version__ to __init__.py

--- orderedmodel/__init__.py
+++ orderedmodel/__init__.py
@@ -1,6 +1,9 @@
 from .models import OrderedModel
 from .admin import OrderedModelAdmin
 
+VERSION = (0, 1, 0)
+__version__ = '.'.join(map(str, VERSION))
+
 __all__ = ['OrderedModel', 'OrderedModelAdmin']
 
 try:
