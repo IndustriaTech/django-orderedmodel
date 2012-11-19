4bdd92d7851a299f8859350746d1731729f3da9b
Venelin Stoykov <venelin@magicbg.com>
2012-11-19 15:04:45 +0200

    Fix for django filer and mptt version lower than 0.5.3

--- orderedmodel/mptt_admin.py
+++ orderedmodel/mptt_admin.py
@@ -3,5 +3,9 @@ from mptt.admin import MPTTModelAdmin
 from .admin import BaseOrderedModelAdmin
 
 
-class OrderedMPTTModelAdmin(MPTTModelAdmin, BaseOrderedModelAdmin):
-    pass
+class OrderedMPTTModelAdmin(BaseOrderedModelAdmin, MPTTModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """
+        Fix for django filer and mptt version lower than 0.5.3
+        """
+        return super(MPTTModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
