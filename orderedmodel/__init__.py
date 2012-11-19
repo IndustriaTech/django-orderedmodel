a6e2c0fc837b17321e2979cb12ba2d0e69603eac
Venelin Stoykov <venelin@magicbg.com>
2012-11-19 12:14:04 +0200

    Make it easy importing of OrderableMPTTModel and OrderedMPTTModelAdmin in from orderedmodel module

--- orderedmodel/__init__.py
+++ orderedmodel/__init__.py
@@ -1,4 +1,14 @@
+from .models import OrderedModel
+from .admin import OrderedModelAdmin
+
 __all__ = ['OrderedModel', 'OrderedModelAdmin']
 
-from models import OrderedModel
-from admin import OrderedModelAdmin
+try:
+    from django.conf import settings
+except ImportError:
+    pass
+else:
+    if 'mptt' in settings.INSTALLED_APPS:
+        from .mptt_models import OrderableMPTTModel
+        from .mptt_admin import OrderedMPTTModelAdmin
+        __all__ += ['OrderableMPTTModel', 'OrderedMPTTModelAdmin']
