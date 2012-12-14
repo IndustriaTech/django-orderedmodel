0fcd797886fa88e23b65dd455c297ee590fc4f3f
Venelin Stoykov <venelin@magicsolutions.bg>
2012-12-14 11:02:41 +0200

    Bump version

--- orderedmodel/__init__.py
+++ orderedmodel/__init__.py
@@ -1,7 +1,7 @@
 from .models import OrderedModel
 from .admin import OrderedModelAdmin
 
-VERSION = (0, 1, 3)
+VERSION = (0, 1, 4)
 __version__ = '.'.join(map(str, VERSION))
 
 __all__ = ['OrderedModel', 'OrderedModelAdmin']
